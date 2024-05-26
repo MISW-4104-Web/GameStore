ce4c44c02f9b72f1ae3b8152f6d3cd3e06e9c7d9
Daniel Escalante Perez <d.escalante@uniandes.edu.co>
2024-05-26 18:42:27 -0500

    Tests of basic entities are done, missing game, review and relations

--- src/minimum-spec/minimum-spec.service.spec.ts
+++ src/minimum-spec/minimum-spec.service.spec.ts
@@ -1,9 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MinimumSpecService } from './minimum-spec.service';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
+import { MinimumSpecEntity } from './minimum-spec.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { faker } from '@faker-js/faker';
 
 describe('MinimumSpecService', () => {
   let service: MinimumSpecService;
+  let repository: Repository<MinimumSpecEntity>;
+  let minimumList: MinimumSpecEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +18,145 @@ describe('MinimumSpecService', () => {
     }).compile();
 
     service = module.get<MinimumSpecService>(MinimumSpecService);
+    repository = module.get<Repository<MinimumSpecEntity>>(getRepositoryToken(MinimumSpecEntity));
+
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+
+    minimumList = [];
+    for (let i = 0; i < 5; i++) {
+      const minimumSpec: MinimumSpecEntity = await repository.save({
+        os: faker.commerce.product(),
+        processor: faker.commerce.product(),
+        videoCard: faker.commerce.product(),
+        ramGb: faker.number.int({min: 1, max: 128}),
+        storageGb: faker.number.int({min: 1, max: 128})
+      });
+      minimumList.push(minimumSpec);
+    }
+
+  };
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('Find All should return all of the entities', async () => {
+    const minimumSpecs: MinimumSpecEntity[] = await service.findAll();
+    expect(minimumSpecs).not.toBeNull();
+    expect(minimumSpecs.length).toBe(5);
+  });
+
+  it('Find One should return the minimum spec with the given id', async () => {
+    const minimumSpec: MinimumSpecEntity = minimumList[0];
+
+    const searchedSpec: MinimumSpecEntity = await service.findOne(minimumSpec.id);
+    expect(searchedSpec).not.toBeNull();
+
+    expect(minimumSpec.os).toEqual(searchedSpec.os);
+    expect(minimumSpec.processor).toEqual(searchedSpec.processor);
+    expect(minimumSpec.ramGb).toEqual(searchedSpec.ramGb);
+    expect(minimumSpec.storageGb).toEqual(searchedSpec.storageGb);
+    expect(minimumSpec.videoCard).toEqual(searchedSpec.videoCard);
+
+  });
+
+  it('Find One should raise an exception when given an invalid id', async () => {
+    await expect(() => service.findOne("0")).rejects.toHaveProperty("message", "The minimum spec with the given id was not found");
+  });
+
+  it('Create should persist the given minimum requirements', async () => {
+    const minimumSpec: MinimumSpecEntity = {
+      id: "",
+      os: faker.commerce.product(),
+      processor: faker.commerce.product(),
+      videoCard: faker.commerce.product(),
+      ramGb: faker.number.int({min: 1, max: 128}),
+      storageGb: faker.number.int({min: 1, max: 128}),
+      games: []
+    };
+
+    const returnedSpec: MinimumSpecEntity = await service.create(minimumSpec);
+    expect(returnedSpec).not.toBeNull();
+
+    const searchedSpec: MinimumSpecEntity = await repository.findOne({
+      where: {id: returnedSpec.id}
+    });
+    expect(searchedSpec).not.toBeNull();
+
+    expect(minimumSpec.os).toEqual(searchedSpec.os);
+    expect(minimumSpec.processor).toEqual(searchedSpec.processor);
+    expect(minimumSpec.ramGb).toEqual(searchedSpec.ramGb);
+    expect(minimumSpec.storageGb).toEqual(searchedSpec.storageGb);
+    expect(minimumSpec.videoCard).toEqual(searchedSpec.videoCard);
+
+    expect(minimumSpec.os).toEqual(returnedSpec.os);
+    expect(minimumSpec.processor).toEqual(returnedSpec.processor);
+    expect(minimumSpec.ramGb).toEqual(returnedSpec.ramGb);
+    expect(minimumSpec.storageGb).toEqual(returnedSpec.storageGb);
+    expect(minimumSpec.videoCard).toEqual(returnedSpec.videoCard);
+  });
+
+  it('Update should persist the changes on a specific minimum requirements', async () => {
+    const minimumSpec: MinimumSpecEntity = minimumList[0];
+
+    minimumSpec.os = "New Os";
+    minimumSpec.processor = "New Processor";
+    minimumSpec.videoCard = "New videocard";
+    minimumSpec.ramGb = 16;
+    minimumSpec.storageGb = 20;
+
+    const returnedSpec: MinimumSpecEntity = await service.update(minimumSpec.id, minimumSpec);
+    expect(returnedSpec).not.toBeNull();
+
+    const searchedSpec: MinimumSpecEntity = await repository.findOne({
+      where: {id: minimumSpec.id}
+    });
+    expect(searchedSpec).not.toBeNull();
+
+    expect(minimumSpec.os).toEqual(searchedSpec.os);
+    expect(minimumSpec.processor).toEqual(searchedSpec.processor);
+    expect(minimumSpec.ramGb).toEqual(searchedSpec.ramGb);
+    expect(minimumSpec.storageGb).toEqual(searchedSpec.storageGb);
+    expect(minimumSpec.videoCard).toEqual(searchedSpec.videoCard);
+
+    expect(minimumSpec.os).toEqual(returnedSpec.os);
+    expect(minimumSpec.processor).toEqual(returnedSpec.processor);
+    expect(minimumSpec.ramGb).toEqual(returnedSpec.ramGb);
+    expect(minimumSpec.storageGb).toEqual(returnedSpec.storageGb);
+    expect(minimumSpec.videoCard).toEqual(returnedSpec.videoCard);
+  });
+
+  it('Update on a given invalid id should raise an exception', async () => {
+    const minimumSpec: MinimumSpecEntity = minimumList[0];
+
+    minimumSpec.os = "New Os";
+    minimumSpec.processor = "New Processor";
+    minimumSpec.videoCard = "New videocard";
+    minimumSpec.ramGb = 16;
+    minimumSpec.storageGb = 20;
+
+    await expect(() => service.update("0", minimumSpec)).rejects.toHaveProperty("message", "The minimum spec with the given id was not found");
+  });
+
+  it('Delete should remove a minimum spec', async () => {
+    const minimumSpec: MinimumSpecEntity = minimumList[0];
+
+    await service.delete(minimumSpec.id);
+
+    const searchedSpec: MinimumSpecEntity = await repository.findOne({
+      where: {id: minimumSpec.id}
+    });
+
+    expect(searchedSpec).toBeNull();
+
+  });
+
+  it('Delete with an invalid id should raise an exception', async () => {
+    await expect(() => service.delete("0")).rejects.toHaveProperty("message", "The minimum spec with the given id was not found");
+  });
+
 });
--- src/tag/tag.service.spec.ts
+++ src/tag/tag.service.spec.ts
@@ -1,9 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TagService } from './tag.service';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
+import { TagEntity } from './tag.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { faker } from '@faker-js/faker';
 
 describe('TagService', () => {
   let service: TagService;
+  let repository: Repository<TagEntity>;
+  let tagList: TagEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +18,115 @@ describe('TagService', () => {
     }).compile();
 
     service = module.get<TagService>(TagService);
+    repository = module.get<Repository<TagEntity>>(getRepositoryToken(TagEntity));
+
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+
+    tagList = [];
+    for (let i = 0; i < 5; i++) {
+      const tag: TagEntity = await repository.save({
+        name: faker.company.name(),
+        description: faker.lorem.sentence()
+      });
+      tagList.push(tag);
+    }
+  };
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('Find all should return all of the tags', async () => {
+    const tags: TagEntity[] = await service.findAll();
+    expect(tags).not.toBeNull();
+    expect(tags.length).toBe(5);
+  });
+
+  it('Find one should return the tag with the given id', async () => {
+    const tag: TagEntity = tagList[0];
+    
+    const searchedTag: TagEntity = await service.findOne(tag.id);
+    expect(searchedTag).not.toBeNull();
+
+    expect(tag.name).toEqual(searchedTag.name);
+    expect(tag.description).toEqual(searchedTag.description);
+
+  });
+
+  it('Find one with an invalid id should raise an exception', async () => {
+    await expect(() => service.findOne("0")).rejects.toHaveProperty("message", "The tag with the given id was not found");
+  });
+
+  it('Create should persist the given tag', async () => {
+    const tag: TagEntity = {
+      id: "",
+      name: faker.company.name(),
+      description: faker.lorem.sentence(),
+      games: []
+    };
+
+    const returnedTag: TagEntity = await service.create(tag);
+    expect(returnedTag).not.toBeNull();
+
+    const searchedTag: TagEntity = await repository.findOne({
+      where: {id: returnedTag.id}
+    });
+
+    expect(tag.name).toEqual(searchedTag.name);
+    expect(tag.description).toEqual(searchedTag.description);
+
+    expect(tag.name).toEqual(returnedTag.name);
+    expect(tag.description).toEqual(returnedTag.description);
+
+  });
+
+  it('Update should persist changes on a tag', async () => {
+    const tag: TagEntity = tagList[0];
+
+    tag.name = "New name";
+    tag.description = "New description";
+
+    const returnedTag: TagEntity = await service.update(tag.id, tag);
+    expect(returnedTag).not.toBeNull();
+
+    const searchedTag: TagEntity = await repository.findOne({
+      where: {id: tag.id}
+    });
+
+    expect(tag.name).toEqual(searchedTag.name);
+    expect(tag.description).toEqual(searchedTag.description);
+
+    expect(tag.name).toEqual(returnedTag.name);
+    expect(tag.description).toEqual(returnedTag.description);
+  });
+
+  it('Update of an entity with an invalid id', async () => {
+    const tag: TagEntity = tagList[0];
+
+    tag.name = "New name";
+    tag.description = "New description";
+
+    await expect(() => service.update("0", tag)).rejects.toHaveProperty("message", "The tag with the given id was not found");
+  });
+
+  it('Delete should remove an entity', async () => {
+    const tag: TagEntity = tagList[0]
+
+    await service.delete(tag.id);
+
+    const searchedTag: TagEntity = await repository.findOne({
+      where: {id: tag.id}
+    });
+
+    expect(searchedTag).toBeNull();
+  });
+
+  it('Delete of an invalid id should raise an exception', async () => {
+    await expect(() => service.delete("0")).rejects.toHaveProperty("message", "The tag with the given id was not found");
+  });
+
 });
